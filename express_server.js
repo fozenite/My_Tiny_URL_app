80cf2a03ee62fa910cf93926425496ca0450e5e2
Rohit Moza <moza.rohit@gmail.com>
2017-04-12 04:12:21 +0000

    URL redirects working as required

--- express_server.js
+++ express_server.js
@@ -38,8 +38,19 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+
+  var myRandomShortURL = generateRandomString();
+  let getURL = req.body.longURL;
+
+  // ADD TO OUR OBJECT
+
+  urlDatabase[myRandomShortURL] = getURL;
+  console.log(urlDatabase);
+  var reDirectPath = "http://localhost:8080/urls/" + myRandomShortURL;
+  console.log(reDirectPath);
+  res.redirect(reDirectPath);
+
+
 });
 
 // ------- /urls Event Handler-------------------//
@@ -51,14 +62,23 @@ app.get("/urls", (req,res) => {
 
 //----------------------------------------------//
 
+// FETCH LONG URL VIA SHORT URL AND FETCH AND REDIRECT TO IT
+app.get("/u/:shortURL", (req, res) => {
+  // let longURL = ...
+  //res.redirect(longURL);
+  let shortURL = req.params.shortURL;
+  let longURL  = urlDatabase[shortURL];
+  //let longURL = urlDatabase[req.params.id.shortURL];
+  res.redirect(longURL);
+
+});
 
 
 // ------ Event handler for displaying a single URL and its shortened  //
 app.get("/urls/:id", (req, res) => {
  let templateVars = { shortURL: req.params.id,
                       longURL: urlDatabase[req.params.id] };
- console.log(req.params);
- console.log(req.params.id);
+
 
  res.render("urls_show", templateVars);
 });
