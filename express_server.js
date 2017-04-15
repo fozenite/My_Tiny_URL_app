4feec8583255d5b7442ff0a7bfad6454cf574942
Rohit Moza <moza.rohit@gmail.com>
2017-04-15 01:52:20 +0000

    Passwords and hashing and functional requirements all met

--- express_server.js
+++ express_server.js
@@ -1,14 +1,18 @@
 var express = require("express");
 
 
-
-var cookieParser = require('cookie-parser')
+var cookieSession = require('cookie-session');  // COOKIE SESSION LIBRARY
+//var cookieParser = require('cookie-parser')
 var app = express()
-app.use(cookieParser())
+// app.use(cookieParser()) //
 var PORT = process.env.PORT || 8080; // default port
 
 // RandomString package //
 var randomString = require("randomstring");
+
+// BECRYPT INITIALIZE FOR OUR PASSWORD
+const bcrypt = require('bcrypt');
+
 
 // Random String function
 function generateRandomString() {
@@ -34,12 +38,12 @@ return myReturnVal;
 
 
 // Check if email and password match
-function CheckEmailAndPassword(emailTest, passwordTest,res) {
+function CheckEmailAndPassword(emailTest, passwordTest,res,req) {
           // CHECK IF EMAIL ALREADY EXISTS
 let myReturnVal = 0;
     Object.keys(users).forEach(key => {
-    if((emailTest == users[key].email)&&(passwordTest == users[key].password)) {
-      res.cookie('user_id',key);
+    if((emailTest == users[key].email)&&( bcrypt.compareSync(passwordTest, users[key].password))) {
+      req.session.user_id = key; // WRITE A VALUE TO COOKIE
       myReturnVal = 1;;
       }
     });
@@ -47,10 +51,10 @@ return myReturnVal;
 }
 
 
-<<<<<<< HEAD
-// LookUp LongURL via ShortURL input
-function lookUpLongURL(shortURL) {
 
+// LookUp LongURL via ShortURL input
+function lookUpLongURL(shortURL,req, reqType) {
+let currentUserCookie = req.session.user_id;
 let myReturnVal = "";
     Object.keys(urlDatabase).forEach(key => {
 
@@ -58,12 +62,14 @@ let myReturnVal = "";
 
         if(shortURL == key2){
           console.log("I found you!!!");
+          if((key == currentUserCookie)||(reqType == "lookup")){
           myReturnVal = urlDatabase[key][key2];
+          } else {
+          myReturnVal = "NotMine";
+          }
         }
       });
-    // if(emailTest == users[key].email){
-    //   myReturnVal = 1;;
-    //   }
+
     });
 return myReturnVal;
 }
@@ -73,29 +79,34 @@ return myReturnVal;
 // Needs to find shortURL match and then replace it's value with
 // new LongURL
 
-function updateMyDatabase(shortURL,longURL) {
-
+function updateMyDatabase(shortURL, longURL,userCookie) {
+  let found = 0;
     Object.keys(urlDatabase).forEach(key => {
 
       Object.keys(urlDatabase[key]).forEach(key2 => {
 
         if(shortURL == key2){
-          console.log("I found you!!!");
+          found = 1;
+          console.log("YOU GOT IT");
           urlDatabase[key][key2]=longURL;
+
         }
       });
     });
+  return found;
 }
-=======
->>>>>>> 8af32c062cc6ddfa2258dfc907ea769cbc8c3fe1
+
 
 // MIDDLE WARE //
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-//------------//
+app.use(cookieSession({
+  name: 'session',
+  keys: ['TinyKey','keyWarrior'],
+  // Cookie Options
+}));
 
 // TELLS EXPRESS TO USE THE EJS TEMPLATING AGENT
 app.set("view engine", "ejs");
@@ -106,29 +117,18 @@ var urlDatabase = {
 
 // LIST OF USERS
 const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
+
 }
 
 // GET NEW URLS HERE --------------------------------------------------//
 app.get("/urls/new", (req, res) => {
-  let currentUserCookie = req.cookies['user_id'];
+  let currentUserCookie = req.session.user_id;
   let templateVars = {user: users[currentUserCookie]};
-<<<<<<< HEAD
+
   if(currentUserCookie){
-=======
->>>>>>> 8af32c062cc6ddfa2258dfc907ea769cbc8c3fe1
-  res.render("urls_new", templateVars);
+  res.status(200).render("urls_new", templateVars);
   } else {
-  res.redirect("/");
+  res.status(401).redirect("/urls");
   }
 });
 // RESPOND TO A POST TO ADD TO OUR DATABASE
@@ -137,17 +137,21 @@ app.post("/urls", (req, res) => {
   // GET A RANDOM 6 CHAR LONG ALPHANUMERIC STRING USING OUR RANDOM STRING PACKAGE
   var myRandomShortURL = generateRandomString();
   // <INPUT> name =longURL </INPUT> From EJS URLS_NEW IS GIVING US THIS
-  let currentUserCookie = req.cookies['user_id'];
+  let currentUserCookie = req.session.user_id;
   let getURL = req.body.longURL;
-
+  if(currentUserCookie){
   // ADD New URL to OUR DATABASE
   urlDatabase[currentUserCookie][myRandomShortURL] = getURL;
+
 
   console.log(urlDatabase);
  // console.log(urlDatabase);
   var reDirectPath = "http://localhost:8080/urls/" + myRandomShortURL;
   // console.log(reDirectPath);
   res.redirect(reDirectPath);
+  } else {
+    res.status(401).redirect("/urls");
+  }
 
 
 });
@@ -172,61 +176,75 @@ app.post("/urls/:id/delete", (req,res) => {
 
 // ------- /urls Event Handler-------------------//
 app.get("/urls", (req,res) => {
-  let currentUserCookie = req.cookies['user_id'];
+  let currentUserCookie = req.session.user_id;
   let templateVars = { urls: urlDatabase,
   user: users[currentUserCookie]};
-  res.render("urls_index", templateVars);
+  if(currentUserCookie){
+    res.status(200).render("urls_index", templateVars);
+  } else {
+    res.status(401).render("urls_index", templateVars);  }
 });
 
 //----------------------------------------------//
 
 // FETCH LONG URL VIA SHORT URL AND FETCH AND REDIRECT TO IT
 app.get("/u/:shortURL", (req, res) => {
-  // let longURL = ...
-  //res.redirect(longURL);
+
   let shortURL = req.params.shortURL;
 
-  //do the lookup for the longURL
-
-
-
+ //do the lookup for the longURL
   //LOOKUP LONG URL
-  let longURL  = lookUpLongURL(shortURL);
-  console.log(longURL);
+  let longURL  = lookUpLongURL(shortURL, req, "lookup");
 
   if(longURL){
   res.redirect(longURL);
   } else{
-    res.send("Invalid request please try again");
+    res.status(404).send("Invalid request please press back on your browser and try again");
   }
 
 });
 
 // Get OUR UPDATE REQUEST
 app.post("/urls/:id", (req,res) =>{
-
+  let currentUserCookie = req.session.user_id;
   let shortURL = req.params.id;
   let longURL  = req.body.longURL;
 
   //DO MY UPDATE
-  updateMyDatabase(shortURL,longURL);
-  //urlDatabase[shortURL] = longURL;
+  let found = updateMyDatabase(shortURL,longURL);
+  if(!found){
+    res.status(404).send("Invalid request please press back on your browser and try again");
+  } else if(!currentUserCookie) {
+    res.status(401).redirect("/urls");
+  } else
 
-
-  res.redirect("http://localhost:8080/urls/");
-
+  res.redirect("/");
 
 });
 
 // ------ Event handler for displaying a single URL and its shortened  //
 app.get("/urls/:id", (req, res) => {
- let currentUserCookie = req.cookies['user_id'];
+ let currentUserCookie = req.session.user_id;
+
+ let longURLf  = lookUpLongURL(req.params.id, req, "update");
   let templateVars = { shortURL: req.params.id,
-                      longURL: urlDatabase[req.params.id],
+                      longURL: longURLf,
                       user: users[currentUserCookie] };
+  console.log(urlDatabase);
 
 
- res.render("urls_show", templateVars);
+//let found = updateMyDatabase(templateVars.shortURL,templateVars.longURL, currentUserCookie);
+  if(!longURLf){
+      if(!currentUserCookie){
+        res.status(401).redirect("/urls");
+      } else {
+        res.status(403).send("Not a valid URL. Press back on your browser and try again");
+      }
+  } else if(longURLf == "NotMine") {
+    res.status(403).send("Error: You don't have update access to this URL");
+  } else {
+    res.status(200).render("urls_show", templateVars);
+  }
 });
 // --------------------------------------------------------------------//
 
@@ -235,9 +253,13 @@ app.get("/urls/:id", (req, res) => {
 
 
 app.get("/login", (req, res) => {
-
-  let templateVars = {user: null};
-  res.render("login",templateVars);
+  let currentUserCookie = req.session.user_id;
+    if(currentUserCookie){
+      res.redirect("/")
+    } else{
+      let templateVars = {user: null};
+      res.status(200).render("login",templateVars);
+    }
 });
 
 
@@ -246,7 +268,7 @@ app.post("/login", (req,res) => {
   let myUserPassword = req.body.password;
 
    let CheckEmail = checkEmailExists(myUserEmail);
-   let CheckLoginWorked = CheckEmailAndPassword(myUserEmail,myUserPassword,res);
+   let CheckLoginWorked = CheckEmailAndPassword(myUserEmail,myUserPassword,res, req);
 
    if(!CheckEmail){
     res.status(403).send("Email address doesn't exist. Please try again or register");
@@ -254,21 +276,14 @@ app.post("/login", (req,res) => {
     res.status(403).send("Incorrect password. Please try again or register");
    } else {
       res.redirect("/");
-
    }
-
-
-
-
 
 });
 // ---------------------------------------------------------------------//
 // LOGOUT
 app.post("/logout", (req,res) => {
 
-  let currentUserCookie = req.cookies['user_id'];
-    //console.log(myCookie);
-  res.clearCookie('user_id', currentUserCookie);
+  req.session = null
 
   res.redirect("/");
 
@@ -277,66 +292,58 @@ app.post("/logout", (req,res) => {
 
 // REGISTER PAGE --------------------------------------------------//
 app.get("/register", (req, res) => {
-
+  if(req.session.user_id){
+    res.redirect("/");
+  } else{
   let templateVars = {user: null};
-  res.render("register",templateVars);
+  res.status(200).render("register",templateVars);
+  }
 });
 
 app.post("/register",(req,res) =>{
   // get random user id
     let user_id = generateRandomString();
+    let hashed_password = bcrypt.hashSync(req.body.password, 10);
 
     let templateVars = {email: req.body.email,
-                      password: req.body.password};
+                      password: hashed_password};
   let CheckEmail = checkEmailExists(templateVars.email);
+
     //CHECK IF EMAIL OR PASSWORD NOT FILLED IN
-  if (!templateVars.email || !templateVars.password){
-    res.status(404).send("You haven't filled in the email or password. Please try again");
-  } else if(CheckEmail){
-    res.status(404).send("We have your email on file, you have already registered. Please login");
-  } else {
-    // Storing User data from register
-       users[user_id] = {
-        id: user_id,
-        email: templateVars.email,
-        password: templateVars.password
-       };
-<<<<<<< HEAD
+        if (!templateVars.email || !templateVars.password){
+          res.status(404).send("You haven't filled in the email or password. Please try again");
+        } else if(CheckEmail){
+          res.status(404).send("We have your email on file, you have already registered. Please login");
+        } else {
+          // Storing User data from register
+             users[user_id] = {
+              id: user_id,
+              email: templateVars.email,
+              password: templateVars.password
+             };
 
-    // SETUP AN EMPTY OBJECT FOR THE NEW USER
-       urlDatabase[user_id] = {};
-=======
->>>>>>> 8af32c062cc6ddfa2258dfc907ea769cbc8c3fe1
-        res.cookie('user_id',user_id);
-        res.redirect("/");
-  }
-
+          // SETUP AN EMPTY OBJECT FOR THE NEW USER
+          urlDatabase[user_id] = {};
+          req.session.user_id = user_id; // WRITE A VALUE TO COOKIE
+          res.redirect("/");
+        }
   });
 
-
+// ROOT PATH GET REQUEST
 app.get("/",(req,res) =>{
-  let currentUserCookie = req.cookies['user_id'];
-  let templateVars = {user: users[currentUserCookie] };
-  res.render("home",templateVars);
-
+  let currentUserCookie = req.session.user_id;
+  // let templateVars = {user: users[currentUserCookie] };
+  // res.render("home",templateVars);
+   // IF CURRENT USER COOKIE EXISTS GO TO URLS PAGE
+  if(currentUserCookie){
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 
-<<<<<<< HEAD
-=======
 
-
-
-
-
-
-
-
-
-
-
->>>>>>> 8af32c062cc6ddfa2258dfc907ea769cbc8c3fe1
-// ----------------------------------------------------------------------//
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
